0b8fcedabb276a2c46cfd8aa1d40753c7f39f544
Kevin Borrill <kevin.borrill@3sidedcube.com>
2017-07-04 16:23:17 +0100

    Create route for login component

---
+++ src/components/auth/Login.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="login">
+    <b-form-fieldset
+      :feedback="feedback"
+      description="Please enter your username"
+      label="Username"
+      :state="state"
+      :label-size="1"
+      >
+
+        <b-form-input v-model="username"></b-form-input>
+
+      </b-form-fieldset>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      username: '',
+      password: ''
+    }
+  },
+  computed: {
+    feedback () {
+      return this.username.length ? '' : 'Please enter something'
+    },
+    state () {
+      return this.username.length ? 'success' : 'warning'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
