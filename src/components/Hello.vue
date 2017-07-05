3c701cd71d1cb8d56e64bb87e9c67070cefc3b7b
Dulcie <dulcie.jackson@3sidedcube.com>
2017-07-05 15:43:33 +0100

    i18n language change tests

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="hello">
-    <h1>{{ $t('hello.test1') }}</h1>
+    <h1 id="changeText">{{ $t('hello.test1') }}</h1>
     <h2>Essential Links</h2>
     <ul>
       <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
