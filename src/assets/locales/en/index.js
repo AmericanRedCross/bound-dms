2f5ed5c1234b3878d3dfeeb3200d9bb8a00bfba0
Dulcie <dulcie.jackson@3sidedcube.com>
2017-07-05 11:47:29 +0100

    Language selection navigation and translation imports/setup

---
+++ src/assets/locales/en/index.js
@@ -0,0 +1,8 @@
+export default{
+  hello: {
+    test1: 'Welcome to Your Vue.js App'
+  },
+  login: {
+    email: 'email'
+  }
+}
---
+++ src/assets/locales/es/index.js
@@ -0,0 +1,8 @@
+export default{
+  hello: {
+    test1: 'Bienvenido a la aplicación Vue.js'
+  },
+  login: {
+    email: 'correo electrónico'
+  }
+}
