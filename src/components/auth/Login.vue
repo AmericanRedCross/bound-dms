6189e4912474b38bf53182b5c63c5f67eef8465a
Dulcie <dulcie.jackson@3sidedcube.com>
2017-07-05 09:20:56 +0100

    Format login components in card and centre

--- src/components/auth/Login.vue
+++ src/components/auth/Login.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="login row justify-content-md-center">
-    <div class="col-6">
+  <div class="login" align="center">
+    <b-card header="Login" style="width: 30rem;">
       <b-form-fieldset
         :feedback="feedback(username)"
         description="Please enter your username"
@@ -8,19 +8,22 @@
         :state="state(username)"
         :label-size="1"
         >
-          <b-form-input v-model="username"></b-form-input>
-      </b-form-fieldset>
 
-      <b-form-fieldset
-        :feedback="feedback(password)"
-        description="Please enter your password"
-        label="Password"
-        :state="state(password)"
-        :label-size="1"
-        >
-          <b-form-input type="password" v-model="password"></b-form-input>
-      </b-form-fieldset>
-    </div>
+          <b-form-input v-model="username"></b-form-input>
+
+        </b-form-fieldset>
+        <b-form-fieldset
+          :feedback="feedback(password)"
+          description="Please enter your password"
+          label="Password"
+          :state="state(password)"
+          :label-size="1"
+          >
+
+            <b-form-input type="password" v-model="password"></b-form-input>
+
+        </b-form-fieldset>
+      </b-card>
   </div>
 </template>
 
