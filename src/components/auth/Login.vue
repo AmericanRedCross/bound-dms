52fb3e2cea7db5d2a69bc8345b49db485f6e858d
Kevin Borrill <kevin.borrill@3sidedcube.com>
2017-07-05 11:02:13 +0100

    Add vee-validate for validation of form elements

--- src/components/auth/Login.vue
+++ src/components/auth/Login.vue
@@ -2,25 +2,21 @@
   <div class="login" align="center">
     <b-card header="Login" style="width: 30rem;">
       <b-form-fieldset
-        :feedback="feedback(username)"
-        description="Please enter your username"
-        label="Username"
-        :state="state(username)"
+        label="Email"
         :label-size="1"
         >
 
-          <b-form-input v-model="username"></b-form-input>
+          <b-form-input v-model="email" class="email" v-validate="'required|email'" name="email" type="text"></b-form-input>
+          <span v-show="errors.has('email')" class="help is-danger">{{ errors.first('email') }}</span>
 
         </b-form-fieldset>
         <b-form-fieldset
-          :feedback="feedback(password)"
-          description="Please enter your password"
           label="Password"
-          :state="state(password)"
           :label-size="1"
           >
 
-            <b-form-input type="password" v-model="password"></b-form-input>
+            <b-form-input v-validate="'required'" name="password" type="password" v-model="password"></b-form-input>
+            <span v-show="errors.has('password')" class="help is-danger">{{ errors.first('password') }}</span>
 
         </b-form-fieldset>
         <b-button>Login</b-button>
@@ -32,7 +28,7 @@
 export default {
   data () {
     return {
-      username: '',
+      email: '',
       password: ''
     }
   },
--- src/main.js
+++ src/main.js
@@ -4,15 +4,16 @@ import Vue from 'vue'
 import BootstrapVue from 'bootstrap-vue'
 import App from './App'
 import router from './router'
+import VeeValidate from 'vee-validate'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 import VueI18n from 'vue-i18n'
 import Locales from './assets/locales'
 
 Vue.use(BootstrapVue)
+Vue.use(VeeValidate)
 Vue.config.productionTip = false
 Vue.use(VueI18n)
-Vue.use(BootstrapVue)
 
 const i18n = new VueI18n({
   locale: 'en',
