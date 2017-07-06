f7d696af3c8ed3805bd56ac5d71cca62c04ef872
Dulcie <dulcie.jackson@3sidedcube.com>
2017-07-06 11:51:53 +0100

    Added vue-chartjs and created dashboard with sample charts

---
+++ src/components/dashboard/BarChart.js
@@ -0,0 +1,11 @@
+import { Bar, mixins } from 'vue-chartjs'
+const { reactiveProp } = mixins
+
+export default Bar.extend({
+  mixins: [reactiveProp],
+  props: ['options'],
+  mounted () {
+    // this.chartData is created in the mixin
+    this.renderChart(this.chartData, this.options)
+  }
+})
---
+++ src/components/dashboard/Dashboard.vue
@@ -0,0 +1,103 @@
+<template>
+  <div class="dashboard">
+    <h1 id="changeText">{{ msg }}</h1>
+      <div class="row justify-content-md-center">
+        <div class="col-3">
+          <b-card header="Test Chart 1">
+            <div class="small">
+              <line-chart :chart-data="datacollection"></line-chart>
+              <button @click="fillData()">Randomize</button>
+            </div>
+          </b-card>
+        </div>
+        <div class="col-3">
+          <b-card header="Test Chart 2">
+            <div class="small">
+              <bar-chart :chart-data="datacollection"></bar-chart>
+              <button @click="fillData()">Randomize</button>
+            </div>
+          </b-card>
+        </div>
+        <div class="col-3">
+          <b-card header="Test Chart 3">
+            <div class="small">
+              <scatter-chart :chart-data="datacollection"></scatter-chart>
+              <button @click="fillData()">Randomize</button>
+            </div>
+          </b-card>
+        </div>
+      </div>
+  </div>
+</template>
+
+<script>
+import LineChart from './LineChart.js'
+import BarChart from './BarChart.js'
+import ScatterChart from './ScatterChart.js'
+
+export default {
+  name: 'dashboard',
+  components: {
+    LineChart,
+    BarChart,
+    ScatterChart
+  },
+  data () {
+    return {
+      msg: 'Dashboard Placeholder',
+      datacollection: null
+    }
+  },
+  mounted () {
+    this.fillData()
+  },
+  methods: {
+    fillData () {
+      this.datacollection = {
+        labels: [this.getRandomInt(), this.getRandomInt()],
+        datasets: [
+          {
+            label: 'Sample Data 1',
+            backgroundColor: '#ff908c',
+            data: [this.getRandomInt(), this.getRandomInt()]
+          }, {
+            label: 'Sample Data 2',
+            backgroundColor: '#fdcb4e',
+            data: [this.getRandomInt(), this.getRandomInt()]
+          }
+        ]
+      }
+    },
+    getRandomInt () {
+      return Math.floor(Math.random() * (50 - 5 + 1)) + 5
+    }
+  }
+}
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+
+.small {
+    max-width: 600px;
+  }
+
+</style>
