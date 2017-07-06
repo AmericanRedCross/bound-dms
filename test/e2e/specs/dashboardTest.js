2be286050889a5557c574d6a8ddf80e1cd920551
Dulcie <dulcie.jackson@3sidedcube.com>
2017-07-06 15:51:57 +0100

    Added to dashboard tests

---
+++ test/e2e/specs/dashboardTest.js
@@ -0,0 +1,69 @@
+// For authoring Nightwatch tests, see
+// http://nightwatchjs.org/guide#usage
+
+module.exports = {
+  'check charts present': function (browser) {
+    // automatically uses dev Server port from /config.index.js
+    // default: http://localhost:8080
+    // see nightwatch.conf.js
+    const devServer = browser.globals.devServerURL
+
+    browser
+      .url(devServer+'/#/dashboard')
+      .waitForElementVisible('.dashboard', 5000)
+      .assert.elementCount('.card', 3)
+      .assert.elementCount('.small', 3)
+      .end()
+  },
+
+  'check french chart translations': function (browser) {
+    // automatically uses dev Server port from /config.index.js
+    // default: http://localhost:8080
+    // see nightwatch.conf.js
+    const devServer = browser.globals.devServerURL
+
+    browser
+      .url(devServer+'/#/dashboard')
+      .waitForElementVisible('.dashboard', 5000)
+      .click('#french')
+      .pause(1000)
+      .assert.containsText('#card1', 'Test Graphique 1')
+      .assert.containsText('#card2', 'Test Graphique 2')
+      .assert.containsText('#card3', 'Test Graphique 3')
+      .end()
+  },
+
+  'check german chart translations': function (browser) {
+    // automatically uses dev Server port from /config.index.js
+    // default: http://localhost:8080
+    // see nightwatch.conf.js
+    const devServer = browser.globals.devServerURL
+
+    browser
+      .url(devServer+'/#/dashboard')
+      .waitForElementVisible('.dashboard', 5000)
+      .click('#german')
+      .pause(1000)
+      .assert.containsText('#card1', 'Testdiagramm 1')
+      .assert.containsText('#card2', 'Testdiagramm 2')
+      .assert.containsText('#card3', 'Testdiagramm 3')
+      .end()
+  },
+
+  'check spanish chart translations': function (browser) {
+    // automatically uses dev Server port from /config.index.js
+    // default: http://localhost:8080
+    // see nightwatch.conf.js
+    const devServer = browser.globals.devServerURL
+
+    browser
+      .url(devServer+'/#/dashboard')
+      .waitForElementVisible('.dashboard', 5000)
+      .click('#spanish')
+      .pause(1000)
+      .assert.containsText('#card1', 'Tabla de Prueba 1')
+      .assert.containsText('#card2', 'Tabla de Prueba 2')
+      .assert.containsText('#card3', 'Tabla de Prueba 3')
+      .end()
+  }
+}
