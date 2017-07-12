fb1be2bda7595624c5f2487fadd1d97bdd2157bb
Richard Baker <richard@3sidedcube.com>
2017-07-12 14:27:33 +0100

    Configure vue-auth token refresh endpoint

--- src/main.js
+++ src/main.js
@@ -37,10 +37,14 @@ Vue.use(VueAuth, {
   http: require('@websanova/vue-auth/drivers/http/axios.1.x.js'),
   router: require('@websanova/vue-auth/drivers/router/vue-router.2.x.js'),
   loginData: {
-    url: 'api/auth',
+    url: 'auth',
     method: 'POST',
     redirect: '/',
     fetchUser: true
+  },
+  refresh: {
+    url: 'auth/refresh',
+    method: 'GET'
   },
   tokenName: 'auth_token'
 })
