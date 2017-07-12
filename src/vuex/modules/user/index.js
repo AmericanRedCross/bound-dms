1ca0ab2191b6c84547ec522acc2130446a8614f7
Kevin Borrill <kevin.borrill@3sidedcube.com>
2017-07-12 09:08:18 +0100

    Add CRUD actions and mutations to Vuex for users

--- src/vuex/modules/user/index.js
+++ src/vuex/modules/user/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const USER_ROOT = 'api/users'
+const USER_ROOT = '/api/users'
 const auth = {
   state: {
     users: [],
@@ -9,19 +9,59 @@ const auth = {
   mutations: {
     SET_USERS: (state, { response }) => {
       state.users = response
+    },
+    SET_USER: (state, { response }) => {
+      // Does the user exist already?
+      let user = state.users.find(user => user.id === response.id)
+      if (user) {
+        user = response
+      } else {
+        state.users.push(response)
+      }
+    },
+    REMOVE_USER: (state, { response }) => {
+      // Find the user index
+      let userIndex = state.users.findIndex(user => user.id === response.id)
+      if (userIndex) {
+        state.users.splice(userIndex, 1)
+      }
     }
   },
   actions: {
-    GET_USERS: function ({ commit }, data) {
+    GET_USERS: function ({ commit }) {
       return axios.get(USER_ROOT).then((response) => {
         commit('SET_USERS', { response: response.data })
       }, (err) => {
-        console.log(err)
+        commit('SET_ERROR', { error: err })
+      })
+    },
+    GET_USER: function ({ commit }, id) {
+      return axios.get(USER_ROOT + '/' + id).then((response) => {
+        commit('SET_USER', { response: response.data })
+      }, (err) => {
+        commit('SET_ERROR', { error: err })
+      })
+    },
+    UPDATE_USER: function ({ commit }, data) {
+      return axios.post(USER_ROOT + '/' + data.id, data).then((response) => {
+        commit('SET_USER', { response: response.data })
+      }, (err) => {
+        commit('SET_ERROR', { error: err })
+      })
+    },
+    DELETE_USER: function ({commit}, id) {
+      return axios.delete(USER_ROOT + '/' + id).then((response) => {
+        commit('REMOVE_USER', { id })
+      }, (err) => {
         commit('SET_ERROR', { error: err })
       })
     }
   },
-  getters: { }
+  getters: {
+    getUserById: (state, getters) => (id) => {
+      return state.users.find(user => user.id === id)
+    }
+  }
 }
 
 export default auth
