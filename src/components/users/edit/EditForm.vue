4b9ed3fda7841ffe8a1795f2b09a325a6a58ece2
Dulcie <dulcie.jackson@3sidedcube.com>
2017-07-13 17:08:07 +0100

    Added form-group class

--- src/components/users/edit/EditForm.vue
+++ src/components/users/edit/EditForm.vue
@@ -5,53 +5,59 @@
         <div>
           {{ $t('users.edit.user') }} {{ user.id }}
         </div>
-        <b-form-fieldset
-          :label="$t('users.edit.firstName')"
-          :label-size="1">
+        <div class="form-group" v-bind:class="{error: $v.firstname.$error}">
+          <b-form-fieldset
+            :label="$t('users.edit.firstName')"
+            :label-size="1">
 
-          <b-form-input
-            v-model="user.firstname"
-            type="text"
-            id="name-input"
-            v-on:input="$v.firstname.$touch"
-            v-bind:class="{error: $v.firstname.$error, valid: $v.firstname.$dirty && !$v.firstname.$invalid}">
-          </b-form-input>
+            <b-form-input
+              v-model="user.firstname"
+              type="text"
+              id="name-input"
+              v-on:input="$v.firstname.$touch"
+              v-bind:class="{error: $v.firstname.$error, valid: $v.firstname.$dirty && !$v.firstname.$invalid}">
+            </b-form-input>
 
-          <span class="form-group__message" v-if="!$v.firstname.required">Field is required</span>
-          <pre>{{ $v }}</pre>
+            <span class="form-group__message" v-if="!$v.firstname.required">Field is required</span>
+            <pre>{{ $v }}</pre>
 
-        </b-form-fieldset>
-        <b-form-fieldset
-          :label="$t('users.edit.lastName')"
-          :label-size="1"
-          >
+          </b-form-fieldset>
+        </div>
+        <div class="form-group" v-bind:class="{error: $v.lastname.$error}">
+          <b-form-fieldset
+            :label="$t('users.edit.lastName')"
+            :label-size="1"
+            >
 
-          <b-form-input
-            v-model="user.lastname"
-            type="text"
-            id="lastname-input"
-            v-on:input="$v.lastname.$touch"
-            v-bind:class="{error: $v.lastname.$error, valid: $v.lastname.$dirty && !$v.lastname.$invalid}">
-          </b-form-input>
+            <b-form-input
+              v-model="user.lastname"
+              type="text"
+              id="lastname-input"
+              v-on:input="$v.lastname.$touch"
+              v-bind:class="{error: $v.lastname.$error, valid: $v.lastname.$dirty && !$v.lastname.$invalid}">
+            </b-form-input>
 
-          <span class="form-group__message" v-if="!$v.lastname.required">Field is required</span>
+            <span class="form-group__message" v-if="!$v.lastname.required">Field is required</span>
 
-        </b-form-fieldset>
-        <b-form-fieldset
-          :label="$t('users.edit.email')"
-          :label-size="1"
-          >
+          </b-form-fieldset>
+        </div>
+        <div class="form-group" v-bind:class="{error: $v.email.$error}">
+          <b-form-fieldset
+            :label="$t('users.edit.email')"
+            :label-size="1"
+            >
 
-          <b-form-input
-            v-model="user.email"
-            type="email"
-            id="email-input"
-            v-on:input="$v.email.$touch"
-            v-bind:class="{error: $v.email.$error, valid: $v.email.$dirty && !$v.email.$invalid}">
-          </b-form-input>
+            <b-form-input
+              v-model="user.email"
+              type="email"
+              id="email-input"
+              v-on:input="$v.email.$touch"
+              v-bind:class="{error: $v.email.$error, valid: $v.email.$dirty && !$v.email.$invalid}">
+            </b-form-input>
 
-          <span class="form-group__message" v-if="!$v.email.required">Field is required</span>
-        </b-form-fieldset>
+            <span class="form-group__message" v-if="!$v.email.required">Field is required</span>
+          </b-form-fieldset>
+        </div>
         <div>
           <label>{{ $t('users.edit.role') }}</label>
           <b-form-select v-model="selected" name="roles" :options="options" class="mb-3" v-validate="'in:admin,translator,content_creator'">
