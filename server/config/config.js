dc423de1e6f2fdf948438814db9b82a3e055f05b
Richard Baker <richard@3sidedcube.com>
2017-07-16 23:22:30 +0100

    Added sequelize package and db config

--- server/config/config.js
+++ server/config/config.js
@@ -1,11 +1,26 @@
 module.exports = {
   production: {
-    jwtSecretKey: process.env.JWT_SECRET || 'secret_key'
+    jwtSecretKey: process.env.JWT_SECRET || 'secret_key',
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    dialect: 'mysql'
   },
   development: {
-    jwtSecretKey: 'secret_key'
+    jwtSecretKey: 'secret_key',
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    dialect: 'mysql'
   },
   testing: {
-    jwtSecretKey: 'secret_key'
+    jwtSecretKey: 'secret_key',
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    dialect: 'mysql'
   }
 }
