1fc037ba2f4dd38f539f3fddbfb08f23fd27ebc3
Richard Baker <richard@3sidedcube.com>
2017-07-17 12:29:30 +0100

    Added user data seeder

---
+++ server/database/seeders/20170717072459-add-users.js
@@ -0,0 +1,33 @@
+const bcrypt = require('bcrypt')
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    /*
+      Add altering commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.bulkInsert('Person', [{
+        name: 'John Doe',
+        isBetaMember: false
+      }], {});
+    */
+    return queryInterface.bulkInsert('Users', [{
+      firstname: 'Test',
+      lastname: 'User',
+      password: bcrypt.hashSync('12345678', 12),
+      isActive: true
+    }], {})
+  },
+
+  down: (queryInterface, Sequelize) => {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.bulkDelete('Person', null, {});
+    */
+    return queryInterface.bulkDelete('Users', null, {})
+  }
+}
