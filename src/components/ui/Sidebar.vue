1071dd3bb277d5788885ff358c51f69135cd48dd
Kevin Borrill <kevin.borrill@3sidedcube.com>
2017-07-18 15:08:26 +0100

    Sidebar styling

--- src/components/ui/Sidebar.vue
+++ src/components/ui/Sidebar.vue
@@ -1,13 +1,13 @@
 <template>
   <div class="sidebar">
     <div class="sidebar-wrapper">
-      <div class="user-logo">
+      <!-- <div class="user-logo">
         <router-link to="dashboard"><img src="../../assets/logo.png"></router-link>
-      </div>
+      </div> -->
       <b-nav vertical>
-        <b-nav-item to="/dashboard"><fa-icon name="pie-chart"></fa-icon>Dashboard</b-nav-item>
-        <b-nav-item to="/users"><fa-icon name="user"></fa-icon>Users</b-nav-item>
-        <b-nav-item to="/documents/edit/"><fa-icon name="pencil"></fa-icon>WYSIWYG</b-nav-item>
+        <b-nav-item to="/dashboard"><span class="sidebar-icon"><fa-icon name="pie-chart"></fa-icon></span>Dashboard</b-nav-item>
+        <b-nav-item to="/users"><span class="sidebar-icon"><fa-icon name="user"></fa-icon></span>Users</b-nav-item>
+        <b-nav-item to="/documents/edit/"><span class="sidebar-icon"><fa-icon name="pencil"></fa-icon></span>WYSIWYG</b-nav-item>
         <b-nav-item>Another Link</b-nav-item>
         <b-nav-item disabled>Disabled</b-nav-item>
       </b-nav>
