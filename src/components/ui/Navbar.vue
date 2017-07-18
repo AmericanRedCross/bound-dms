a869f9c790d726cf94ac7f65073f7d3ae0e54706
Kevin Borrill <kevin.borrill@3sidedcube.com>
2017-07-18 14:36:32 +0100

    Move sidebar and navbar styling to their own sass files

--- src/components/ui/Navbar.vue
+++ src/components/ui/Navbar.vue
@@ -51,16 +51,3 @@ export default {
   }
 }
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style lang="scss" scoped>
-  .navbar {
-    position: fixed;
-    top: 0;
-    width: 100%;
-    z-index: 10;
-  }
-  .selected {
-    background-color: #4aabff;
-  }
-</style>
