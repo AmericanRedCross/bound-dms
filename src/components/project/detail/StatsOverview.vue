7ee313900fd9428b001b27e4abdd9e105201867b
Kevin Borrill <kevin.borrill@3sidedcube.com>
2017-07-24 10:05:03 +0100

    Statistic Boxes

--- src/components/project/detail/StatsOverview.vue
+++ src/components/project/detail/StatsOverview.vue
@@ -1,33 +1,38 @@
 <template>
     <div class="row">
       <div class="col-md-12">
-        <b-card :title="project.name"><small>Owned by Kevin Borrill</small></b-card>
-        <div class="row">
-          <div class="col-md-3">
-
+        <b-card :title="project.name">
+          <h1>Project Name</h1><small>Owned by Kevin Borrill</small>
+          <div class="row">
+            <div class="col-md-3 col-sm-6 m-b-10">
+              <StatBox :value="50" type="%" description="A description here" colour="#20A8D8" inverse></StatBox>
+            </div>
+            <div class="col-md-3 col-sm-6 m-b-10">
+              <StatBox :value="30" type="%" description="A description here" colour="#FF7914" inverse></StatBox>
+            </div>
+            <div class="col-md-3 col-sm-6 m-b-10">
+              <StatBox :value="75" type="%" description="A description here" colour="#8ED820"></StatBox>
+            </div>
+            <div class="col-md-3 col-sm-6 m-b-10">
+              <StatBox :value="100" type="%" description="A description here" colour="#D82069" inverse></StatBox>
+            </div>
           </div>
-          <div class="col-md-3">
-
-          </div>
-          <div class="col-md-3">
-
-          </div>
-          <div class="col-md-3">
-
-          </div>
-        </div>
+        </b-card>
       </div>
     </div>
 </template>
 
 <script>
-
+import StatBox from '../../ui/StatBox'
 export default {
   name: 'stats-overview',
   props: {
     project: {
       type: Object
     }
+  },
+  components: {
+    StatBox
   }
 }
 </script>
---
+++ src/components/ui/Statbox.vue
@@ -0,0 +1,45 @@
+<template>
+  <b-card :style="'background-color:' + colour" :class="{inverse: inverse}">
+    <h2><slot name="value">{{ value }}{{type}}</slot></h2>
+    <small><slot name="description">{{ description }}</slot></small>
+    <b-progress v-model="value" :precision="1"></b-progress>
+  </b-card>
+</template>
+<script>
+
+export default {
+  name: 'stat-box',
+  props: {
+    value: {
+      type: Number,
+      default: 0
+    },
+    type: {
+      type: String,
+      default: '%'
+    },
+    description: {
+      type: String
+    },
+    colour: {
+      type: String,
+      default: '#FFF'
+    },
+    inverse: {
+      type: Boolean,
+      default: false
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  @import "../../assets/sass/utilities/mixins.scss";
+ .inverse {
+   color: white;
+ }
+ .card {
+   border: none;
+   @include no-select();
+ }
+</style>
