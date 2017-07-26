5cafff3d407881d6fa5e8242bc10d45c95d4809e
Kevin Borrill <kevin.borrill@3sidedcube.com>
2017-07-26 14:12:22 +0100

    Restructure Projects Routing:  - Move Projects into a Projects Container  - Have a project with its own container so it can load in the project overview / structure / translations etc... as children

---
+++ src/components/structure/Module.vue
@@ -0,0 +1,22 @@
+<template>
+  <b-card>
+    Module
+  </b-card>
+</template>
+<script>
+import { Step } from '../../vuex/modules/structure/Step'
+
+export default {
+  name: 'module',
+  props: {
+    module: {
+      type: Object,
+      default: new Step({})
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/components/structure/Structure.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="structure">
+    <Module></Module>
+  </div>
+</template>
+<script>
+import Module from './Module'
+
+export default {
+  name: 'Structure',
+  components: {
+    Module
+  },
+  props: {
+    project: {
+      type: Object
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
