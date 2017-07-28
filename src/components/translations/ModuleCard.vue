e3ce5b315550c92d2d915a812129659e35cb60cc
Dulcie <dulcie.jackson@3sidedcube.com>
2017-07-28 15:28:44 +0100

    Created module card structure and added to translation workflow page

---
+++ src/components/translations/ModuleCard.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="row" align="center">
+      <div class="col">
+        <b-card>
+          <div class="row">
+            <div class="col-md-6">
+              Module Title: English Title
+            </div>
+            <span>
+            <div class="col-md-6">
+              <b-form-input type="text" placeholder="Translated Module Name"></b-form-input>
+            </div>
+            </span>
+          </div>
+        </b-card>
+      </div>
+    </div>
+</template>
--- src/components/translations/TranslationWorkflow.vue
+++ src/components/translations/TranslationWorkflow.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="translation-workflow">
+    <div class="translation-workflow" align="center">
       <div class="visible-toggle" align="center">
         <b-button>All Content</b-button>
         <b-button>Needs Translation</b-button>
@@ -8,20 +8,50 @@
       <br />
       <div class="translation-info" align="center">
         <span>Base Language</span>
+        <span class="ml-auto">
+          <b-dropdown class="m-md-2" right text="EN">
+            <b-dropdown-item-button>
+              EN
+            </b-dropdown-item-button>
+          </b-dropdown>
+        </span>
+        <span>
+          <fa-icon name="play"></fa-icon>
+        </span>
+        <span class="ml-auto">
+          <b-dropdown class="m-md-2" right text="ESP">
+            <b-dropdown-item-button>
+              ESP
+            </b-dropdown-item-button>
+          </b-dropdown>
+        </span>
+        <span>Translation Language</span>
+      </div>
+      <div class="translation-progress col-md-3" align="center">
+        <b-progress v-model="translated" :precision="1" animated></b-progress>
+        <small>
+        {{ translated }}% of content translated
+        </small>
+      </div>
+      <div>
+        <ModuleCard></ModuleCard>
       </div>
     </div>
 </template>
 
 <script>
 import { mapGetters } from 'vuex'
+import ModuleCard from '@/components/translations/ModuleCard'
 
 export default {
   name: 'translation-workflow',
   components: {
+    ModuleCard
   },
   data () {
     return {
-      project: null
+      project: null,
+      translated: 23
     }
   },
   computed: {
