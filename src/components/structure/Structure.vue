acadf61eb6ee0b7bb1ff82f165039a21b6af64c0
Dulcie <dulcie.jackson@3sidedcube.com>
2017-08-08 14:16:02 +0100

    Started creating language change dropdown

--- src/components/structure/Structure.vue
+++ src/components/structure/Structure.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="structure">
+    <v-select :options="projectLangOptions"></v-select>
     <b-button @click.native="saveRevision" variant="success">Save Revision</b-button>
     <draggable v-model="structure" @update="updateDraggable">
       <StepComp v-for="module in structure" :key="module.id" :step="module" :isModule="true"></StepComp>
@@ -10,12 +11,20 @@
 /** TODO: Refactor this (along with steps) so that we don't have duplicated update code, i.e. setting the structure + hierarchies on drag. */
 import StepComp from './Step'
 import draggable from 'vuedraggable'
+import { languages } from 'countries-list'
+import vSelect from 'vue-select'
 
 export default {
   name: 'Structure',
   components: {
     StepComp,
-    draggable
+    draggable,
+    vSelect
+  },
+  props: {
+    project: {
+      type: Object
+    }
   },
   mounted () {
     this.$store.dispatch('GET_STRUCTURE', this.$route.params.id)
@@ -41,11 +50,11 @@ export default {
       set (value) {
         this.$store.dispatch('UPDATE_STRUCTURE', value)
       }
-    }
-  },
-  props: {
-    project: {
-      type: Object
+    },
+    projectLangOptions () {
+      return this.project.languages.map((language) => {
+        return { label: `${languages[language.code].name} (${language.code})`, value: language.code }
+      })
     }
   }
 }
