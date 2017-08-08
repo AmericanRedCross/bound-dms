05b8669614525d642e17cc1b36f428bf088dcdd2
Dulcie <dulcie.jackson@3sidedcube.com>
2017-08-08 14:19:31 +0100

    Pull in current project

--- src/components/structure/Structure.vue
+++ src/components/structure/Structure.vue
@@ -26,8 +26,23 @@ export default {
       type: Object
     }
   },
+  data () {
+    return {
+      project: new Project({})
+    }
+  },
   mounted () {
     this.$store.dispatch('GET_STRUCTURE', this.$route.params.id)
+  },
+  beforeMount () {
+    // Call vuex to retrieve the current project from the backend. This returns a promise so we know when it's finished.
+    this.$store.dispatch('GET_PROJECT', this.$route.params.id).then(() => {
+     // Get the project that was just retrieved (the getProjectById getter is from the vuex getter, there's a special helper
+     // called 'mapGetters' in the computed section of this component that gets the project from the vuex state.)
+      let project = this.getProjectById(parseInt(this.$route.params.id), 10)
+    // Set the project so the component can see it
+      this.project = project
+    })
   },
   methods: {
     saveRevision () {
