87dfbe4e81bc392e87670f021ae37dc14740af46
Richard Baker <richard@3sidedcube.com>
2017-08-11 08:52:49 +0100

    Add project link to sidebar

--- src/components/ui/Sidebar.vue
+++ src/components/ui/Sidebar.vue
@@ -5,7 +5,7 @@
         <router-link to="dashboard"><img src="../../assets/logo.png"></router-link>
       </div> -->
       <b-nav v-if="projectId" vertical>
-        <b-nav-item to="/dashboard"><span class="sidebar-icon"><fa-icon name="pie-chart"></fa-icon></span>{{ $t('sidebar.dashboard')}}</b-nav-item>
+        <b-nav-item :to="{name: 'project-detail', params: {id: projectId}}"><span class="sidebar-icon"><fa-icon name="pie-chart"></fa-icon></span>{{ $t('sidebar.dashboard')}}</b-nav-item>
         <b-nav-item to="/projects"><span class="sidebar-icon"><fa-icon name="folder" ></fa-icon></span>Directories</b-nav-item>
         <b-nav-item to="/projects"><span class="sidebar-icon"><fa-icon name="file" ></fa-icon></span>Documents</b-nav-item>
         <b-nav-item to="/projects/projectId"><span class="sidebar-icon"><fa-icon name="cloud-upload" ></fa-icon></span>Publish</b-nav-item>
