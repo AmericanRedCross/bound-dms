0e4e22b0d83a1738718c4b92565d039721b6d252
Kevin Borrill <kevin.borrill@3sidedcube.com>
2017-08-22 11:56:28 +0100

    Move bootstrap imports to top of main.js

--- src/main.js
+++ src/main.js
@@ -1,3 +1,7 @@
+import BootstrapVue from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
@@ -7,9 +11,6 @@ import router from './router'
 import store from './vuex'
 import Locales from './assets/locales'
 
-import BootstrapVue from 'bootstrap-vue'
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
 import 'vue-awesome/icons'
 import Icon from 'vue-awesome/components/Icon'
 import Vuelidate from 'vuelidate'
