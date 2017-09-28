8aa3904fa21ac35605b27ca43e30ed957ecb7f64
Chris Sherry <chris.sherry@3sidedcube.com>
2017-09-28 17:36:29 +0100

    Add meta data to directory get endpoint

---
+++ server/transformers/directory.js
@@ -0,0 +1,28 @@
+module.exports = {
+  transform: (directory) => {
+
+    let metadata = {}
+    directory.metatypes.forEach((metatype, index) => {
+
+      let value = metatype.MetaValue.value
+      if (metatype.type === 'boolean') {
+        value = (value === '1')
+      } else if (metatype.type === 'integer') {
+        value = parseInt(value)
+      }
+      metadata[metatype.key] = value
+    })
+
+    return {
+      id: directory.id,
+      projectId: directory.projectId,
+      order: directory.order,
+      createdAt: directory.createdAt,
+      updatedAt: directory.updatedAt,
+      createdBy: directory.createdBy,
+      directories: directory.directories,
+      translations: directory.translations,
+      metadata: metadata
+    }
+  }
+}
