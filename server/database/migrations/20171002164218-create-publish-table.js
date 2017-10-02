4fb386bee5275d4f0cdb31278d849836fd9e636e
Richard Baker <richard@3sidedcube.com>
2017-10-02 18:10:33 +0100

    Adds publish model and migration

---
+++ server/database/migrations/20171002164218-create-publish-table.js
@@ -0,0 +1,62 @@
+module.exports = {
+  up: function (queryInterface, Sequelize) {
+    return queryInterface.createTable('Publishes', {
+      id: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+      },
+      projectId: {
+        allowNull: false,
+        type: Sequelize.INTEGER
+      },
+      type: {
+        allowNull: false,
+        type: Sequelize.STRING()
+      },
+      filePath: {
+        allowNull: false,
+        type: Sequelize.STRING()
+      },
+      createdById: {
+        allowNull: true,
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal('NOW()')
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal('NOW()')
+      }
+    },
+      {
+        charset: 'utf8mb4'
+      }
+    ).then(() => {
+      return queryInterface.addConstraint('Publishes', ['projectId'], {
+        type: 'FOREIGN KEY',
+        references: {
+          table: 'Projects',
+          field: 'id'
+        }
+      })
+    }).then(() => {
+      return queryInterface.addConstraint('Publishes', ['createdById'], {
+        type: 'FOREIGN KEY',
+        references: {
+          table: 'Users',
+          field: 'id'
+        }
+      })
+    })
+  },
+
+  down: function (queryInterface, Sequelize) {
+    return queryInterface.dropTable('Publishes')
+  }
+}
---
+++ server/models/publish.js
@@ -0,0 +1,28 @@
+module.exports = (sequelize, DataTypes) => {
+  const Publish = sequelize.define('Publish', {
+    projectId: {type: DataTypes.INTEGER, allowNull: false},
+    type: {type: DataTypes.STRING, allowNull: false},
+    filePath: {type: DataTypes.STRING, allowNull: false},
+    createdById: {type: DataTypes.INTEGER, allowNull: true}
+  })
+
+  Publish.massAssignable = function () {
+    return ['projectId', 'type', 'filePath', 'createdById']
+  }
+
+  Publish.associate = (models) => {
+    Publish.belongsTo(models.Project, {
+      as: 'project',
+      foreignKey: 'projectId',
+      targetKey: 'id'
+    })
+
+    Publish.belongsTo(models.User, {
+      as: 'createdBy',
+      foreignKey: 'createdById',
+      targetKey: 'id'
+    })
+  }
+
+  return Publish
+}
