af244788a6221392aea59a685217af7301f08324
Kevin Borrill <kevin.borrill@3sidedcube.com>
2017-10-08 14:55:10 +0100

    Switch over to bootstrap scss

--- src/main.js
+++ src/main.js
@@ -1,6 +1,4 @@
 import BootstrapVue from 'bootstrap-vue'
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
