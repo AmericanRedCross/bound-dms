ac0de41a586ffa823970ed5e5f7769f181d9ba76
Kevin Borrill <kevin.borrill@3sidedcube.com>
2017-10-12 15:57:22 +0100

    Skeleton fields in a card for when we are loading a project.

--- src/components/project/Project.vue
+++ src/components/project/Project.vue
@@ -1,6 +1,19 @@
 <template>
   <div class="project">
-    <router-view></router-view>
+    <router-view v-show="!fetchingProject"></router-view>
+    <b-row class="loading" align-h="center" v-show="fetchingProject">
+      <b-col class="text-center">
+        <b-card>
+          <div class="skeleton" style="width: 200px; height: 20px"></div>
+          <br>
+          <div class="skeleton mt-1" style="width: 300px; height: 10px"></div>
+          <div class="skeleton mt-1" style="width: 400px; height: 10px"></div>
+          <div class="skeleton mt-1" style="width: 200px; height: 10px"></div>
+          <div class="skeleton mt-1" style="width: 300px; height: 10px"></div>
+          <div class="skeleton mt-2" style="width: 50px; height: 50px"></div>
+        </b-card>
+      </b-col>
+    </b-row>
   </div>
 </template>
 <script>
@@ -10,7 +23,8 @@ export default {
   name: 'project-container',
   data () {
     return {
-      project: null
+      project: null,
+      fetchingProject: false
     }
   },
   watch: {
@@ -22,6 +36,7 @@ export default {
   },
   methods: {
     fetchProject () {
+      this.fetchingProject = true
       this.$store.dispatch('GET_PROJECT', this.$route.params.id).then(() => {
         this.project = this.getProjectById(parseInt(this.$route.params.id))
         let baseLang = this.project.languages.find(lang => lang.code === this.project.baseLanguage)
@@ -29,7 +44,9 @@ export default {
           this.$store.dispatch('CHANGE_BASE_LANGUAGE', baseLang)
         }
         this.$store.dispatch('CHANGE_AVAILABLE_LANGUAGES', this.project.languages)
+        this.fetchingProject = false
       }).catch(() => {
+        this.fetchingProject = false
         this.$notifications.notify(
           {
             message: `<b>${this._i18n.t('common.oops')}</b><br /> ${this._i18n.t('common.error')}`,
